a03f4363fd989122c2fd907e32baeee4c83aa911
July0515 <17805597915@163.com>
2021-05-24 03:18:16 +0800

    Initial commit

---
+++ data/user_manager_data.py
@@ -0,0 +1,9 @@
+
+# 主要用来管理用户数据
+
+class UserManagerData():
+    user_case1_data = {
+        "username":"testk03",
+        "password":"testko3",
+        "new_user":"testk04"
+    }
---
+++ utils/common_utils.py
@@ -0,0 +1,4 @@
+
+# 实现公共断言
+def common_assert(obj,result,except_result=0):
+    obj.assertEqual(except_result,result['errno'])
